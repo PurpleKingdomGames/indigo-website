d60d047fac4fb1bc98d7d6215fa4ebfcf2ab1552
Dave Smith <davesmith.00000@gmail.com>
2021-09-07 23:09:24 +0100

    Added API docs

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -43,7 +43,7 @@ const siteConfig = {
     { blog: true, label: 'Blog' },
     { doc: 'guides', label: 'Guides' },
     { doc: 'development-status', label: 'Docs' },
-    { href: "https://purplekingdomgames.github.io/indigo/api/indigo.html", label: "APIs", external: true },
+    { href: "/api/docs/", label: "APIs", external: true },
     { page: 'tools', label: 'Tools' },
     { href: "https://github.com/PurpleKingdomGames/indigo", label: "GitHub", external: true },
   ],
